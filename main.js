4db65609edb62b073b86cb0a25b429091affe628
darkmah <39097475+darkmah@users.noreply.github.com>
2021-04-03 00:51:57 +0200

    fixed function name , added shuffle

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 //imports
-require('dotenv').config();
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
-const { MessageEmbed } = require('discord.js');
-const fs = require('fs');
+require("dotenv").config();
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
 //setup
 const client = new Discord.Client();
 //variables
-const prefix = '-';
+const prefix = "-";
 var playlist = [];
 var currentSong = 0;
 var channel = null;
 var vc = null;
 var loop = false;
 
-client.on('ready', () => {
-  console.log('bot online');
+client.on("ready", () => {
+  console.log("bot online");
 });
-client.on('message', (message) => {
+client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
-  if (command === 'play') {
+  if (command === "play") {
     // Checking if the message author is in a voice channel.
 
     if (!message.member.voice.channel)
-      return message.reply('You must be in a voice channel.');
+      return message.reply("You must be in a voice channel.");
     // Checking if the bot is in a voice channel.
     if (message.guild.me.voice.channel)
       return message.reply("I'm already playing.");
 
     channel = message.member.voice.channel;
-    playlist = getFiles('./Music');
-    playlist = playlist.filter((song) => !song.includes('.gitkeep'));
+    playlist = getFiles("./Music");
+    playlist = playlist.filter((song) => !song.includes(".gitkeep"));
     if (playlist.length == 0) {
-      return message.reply('Playlist Empty');
+      return message.reply("Playlist Empty");
     }
 
     // Joining the channel and creating a VoiceConnection.
@@ -50,64 +50,77 @@ client.on('message', (message) => {
         VoiceConnection.disconnect();
       });
   }
-  if (command === 'choose') {
+  if (command === "choose") {
     //console.log(args);
     if (args.length >= 1 && !Number.isInteger(parseInt(args[0]))) {
-      return message.reply('Invalid Number.');
+      return message.reply("Invalid Number.");
     }
     if (args.length >= 1) {
       currentSong = args[0] - 1;
       if (currentSong < 0 || currentSong >= playlist.length) {
-        return message.reply('Song doesnt belong to playlist.');
+        return message.reply("Song doesnt belong to playlist.");
       } else {
         playSong(vc, message);
       }
     }
   }
-  if (command === 'next') {
+  if (command === "next") {
     currentSong = (currentSong + 1) % playlist.length;
     playSong(vc, message);
     //console.log(playlist);
   }
-  if (command === 'prev') {
+  if (command === "prev") {
     if (currentSong - 1 < 0) {
       currentSong = playlist.length - 1;
     }
     playSong(vc, message);
   }
-  if (command === 'loop') {
+  if (command === "loop") {
     loop = !loop;
     message.reply(`loop is ${loop}`);
   }
-  if (command === 'leave') {
+  if (command === "leave") {
     //message.member.voice.channel.leave();
     if (channel != null) {
       channel.leave();
       channel = null;
     }
   }
-  if (command === 'queue') {
-    const embed = new MessageEmbed().setTitle('Current Queue');
+  if (command === "queue") {
+    const embed = new MessageEmbed().setTitle("Current Queue");
     var i = 0;
     playlist.forEach((song) => {
       embed.addField(
         `${i + 1} - `,
-        `${playlist[i].match(/([^\/]*)\/*$/)[1].replace('.mp3', '')}.`
+        `${playlist[i].match(/([^\/]*)\/*$/)[1].replace(".mp3", "")}.`
       );
       i++;
     });
     message.reply(embed);
   }
-  if (command === 'meme') {
+  if (command === "meme") {
     getMeme(message);
+  }
+  if (command === "shuffle") {
+    if (playlist.length > 0) {
+      shuffleArray(playlist);
+    } else {
+      message.reply("Playlist Empty");
+    }
   }
 });
 //functions
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 async function getMeme(message) {
-  fetch('https://meme-api.herokuapp.com/gimme')
+  fetch("https://meme-api.herokuapp.com/gimme")
     .then((res) => res.json())
     .then(async (json) => {
-      let msg = await message.channel.send('Fetching you a juicy meme!');
+      let msg = await message.channel.send("Fetching you a juicy meme!");
       const embed = new MessageEmbed()
         .setTitle(json.title)
         .setImage(json.url)
@@ -119,11 +132,13 @@ function getFiles(dir) {
   var results = [];
   var list = fs.readdirSync(dir);
   list.forEach(function (file) {
-    file = dir + '/' + file;
+    file = dir + "/" + file;
     var stat = fs.statSync(file);
     if (stat && stat.isDirectory()) {
       /* Recurse into a subdirectory */
-      results = results.concat(walk(file));
+      console.log(results);
+      results = results.concat(getFiles(file));
+      console.log(results);
     } else {
       /* Is a file */
       results.push(file);
@@ -136,9 +151,9 @@ function playSong(VoiceConnection, message) {
   message.reply(
     `Playing ${playlist[currentSong]
       .match(/([^\/]*)\/*$/)[1]
-      .replace('.mp3', '')}.`
+      .replace(".mp3", "")}.`
   );
-  VoiceConnection.play(playlist[currentSong]).on('finish', () => {
+  VoiceConnection.play(playlist[currentSong]).on("finish", () => {
     currentSong++;
     if (currentSong < playlist.length || loop) {
       playSong(VoiceConnection, message);
