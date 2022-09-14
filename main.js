836dbb0579516e5228ca6641f825478b0111f82e
Mahmoud Okasha <mahmoud_okasha17@hotmail.com>
2022-09-14 19:14:17 +0200

    updated dependencies

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ client.on('ready', () => {
 client.on('message', (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(/ +/);
+
   const command = args.shift().toLowerCase();
   if (command === 'play') {
     // Checking if the message author is in a voice channel.
